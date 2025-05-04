3de68a739f4679cfe5402ee0ede01bbdf983c69d
Darek Mazur <dev@darekmazur.it>
2025-05-05 00:31:24 +0200

    Update tests for Root component

--- src/Root.test.tsx
+++ src/Root.test.tsx
@@ -32,6 +32,19 @@ describe('Root', () => {
     expect(screen.getByText(/tolkienarium ©/i)).toBeInTheDocument();
   });
 
+  it('renders a Login Page for /login path', () => {
+    render(
+      <MemoryRouter initialEntries={['/login']}>
+        <Root />
+      </MemoryRouter>,
+    );
+    expect(screen.getByText(/login/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/username/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/username/i)).toBeRequired();
+    expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/password/i)).toBeRequired();
+  });
+
   it('matches the snapshot for the default path', () => {
     const { asFragment } = render(
       <MemoryRouter initialEntries={['/']}>
