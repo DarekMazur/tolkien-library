4deec15961534a4a03ce0a900a3b669ebc97c0b1
Darek Mazur <dev@darekmazur.it>
2025-05-07 07:33:50 +0200

    Create page for unauthorized user

---
+++ src/components/pages/UnauthorizedView/UnauthorizedView.tsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from '@mui/material';
+
+const UnauthorizedView = () => {
+  return (
+    <Box>
+      <Typography>You Shall Not Pass!</Typography>
+    </Box>
+  );
+};
+
+export default UnauthorizedView;
