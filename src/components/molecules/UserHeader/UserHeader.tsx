52f382984c92710232607afb59f8c8c8b2df0cc0
Darek Mazur <dev@darekmazur.it>
2025-06-21 03:48:31 +0200

    Move header to new component

---
+++ src/components/molecules/UserHeader/UserHeader.tsx
@@ -0,0 +1,28 @@
+import { IconButton, Paper, Typography } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import { SetStateAction } from 'react';
+import { userHeaderWrapperStyles } from '@/components/molecules/UserHeader/UserHeader.styles.ts';
+
+interface IUserHeaderProps {
+  editMode: boolean;
+  setEditMode: (value: SetStateAction<boolean>) => void;
+}
+
+const UserHeader = ({ editMode, setEditMode }: IUserHeaderProps) => {
+  return (
+    <Paper sx={userHeaderWrapperStyles}>
+      <Typography variant="h4">Your profile</Typography>
+      <Typography variant="subtitle1">Manage your data</Typography>
+      {!editMode && (
+        <IconButton
+          sx={{ position: 'absolute', right: 14, top: 14 }}
+          onClick={() => setEditMode(true)}
+        >
+          <EditIcon fontSize="large" />
+        </IconButton>
+      )}
+    </Paper>
+  );
+};
+
+export default UserHeader;
--- src/components/pages/UserProfile/UserProfile.tsx
+++ src/components/pages/UserProfile/UserProfile.tsx
@@ -1,10 +1,10 @@
-import { Card, CardContent, Typography, Paper, IconButton, Snackbar, Alert } from '@mui/material';
+import { Card, CardContent, Snackbar, Alert } from '@mui/material';
 import Wrapper from '@/components/atoms/Wrapper/Wrapper.tsx';
 import { useMe } from '@/hooks/useMe.tsx';
 import Loader from '@/components/atoms/Loader/Loader.tsx';
-import EditIcon from '@mui/icons-material/Edit';
 import { useState } from 'react';
 import UserForm from '@/components/molecules/UserForm/UserForm.tsx';
+import UserHeader from '@/components/molecules/UserHeader/UserHeader.tsx';
 
 const UserProfile = () => {
   const { user, isLoading } = useMe();
@@ -25,27 +25,7 @@ const UserProfile = () => {
     <Wrapper>
       {user ? (
         <Card sx={{ maxWidth: 800, margin: '0 auto', borderRadius: 2, overflow: 'hidden' }}>
-          <Paper
-            sx={{
-              p: 3,
-              bgcolor: 'primary.light',
-              color: 'primary.contrastText',
-              borderRadius: 0,
-              position: 'relative',
-            }}
-          >
-            <Typography variant="h4">Your profile</Typography>
-            <Typography variant="subtitle1">Manage your data</Typography>
-            {!editMode && (
-              <IconButton
-                sx={{ position: 'absolute', right: 14, top: 14 }}
-                onClick={() => setEditMode(true)}
-              >
-                <EditIcon fontSize="large" />
-              </IconButton>
-            )}
-          </Paper>
-
+          <UserHeader editMode={editMode} setEditMode={setEditMode} />
           <CardContent sx={{ p: 4 }}>
             <UserForm
               user={user}
