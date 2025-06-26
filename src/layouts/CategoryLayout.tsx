4250ace22563b3ec5653fb845507181dc4dcc3fd
Darek Mazur <dev@darekmazur.it>
2025-06-26 23:22:19 +0200

    Create hardcoded category mock

--- src/layouts/CategoryLayout.tsx
+++ src/layouts/CategoryLayout.tsx
@@ -1,6 +1,41 @@
 import Wrapper from '@/components/atoms/Wrapper/Wrapper.tsx';
 import { Typography } from '@mui/material';
 import { useLocation } from 'react-router';
+import { faker } from '@faker-js/faker';
+import slugify from 'slugify';
+
+const createSlug = (title: string) => {
+  return slugify(title, {
+    replacement: '-',
+    remove: /[*+~.()'"!:@]/g,
+    lower: true,
+    strict: true,
+    locale: 'pl',
+    trim: true,
+  });
+};
+
+const categoryTitle = faker.lorem.words({ min: 1, max: 2 });
+
+const mockedCategory = {
+  id: faker.string.uuid(),
+  title: categoryTitle,
+  slug: createSlug(categoryTitle),
+  pages: [
+    {
+      title: faker.lorem.words({ min: 1, max: 4 }),
+    },
+    {
+      title: faker.lorem.words({ min: 1, max: 4 }),
+    },
+    {
+      title: faker.lorem.words({ min: 1, max: 4 }),
+    },
+    {
+      title: faker.lorem.words({ min: 1, max: 4 }),
+    },
+  ],
+};
 
 const CategoryLayout = () => {
   const location = useLocation();
@@ -11,7 +46,12 @@ const CategoryLayout = () => {
 
   return (
     <Wrapper>
-      <Typography variant="h2">{categorySlug}</Typography>
+      <Typography variant="h2">{mockedCategory.title}</Typography>
+      {mockedCategory.pages.map((page) => (
+        <Typography>
+          <a href={`/library/${categorySlug}/${createSlug(page.title)}`}>{page.title}</a>
+        </Typography>
+      ))}
     </Wrapper>
   );
 };
