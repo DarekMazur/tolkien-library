c10ed37bc58adca67277fff61d1f43b02a3c6351
Darek Mazur <dev@darekmazur.it>
2025-06-30 16:59:27 +0200

    Remove unused method from getDataFromApi helper

--- src/lib/getDataFromApi.ts
+++ src/lib/getDataFromApi.ts
@@ -1,26 +1,4 @@
-import { ICategoryProps, IRegisteredUser } from '@/lib/types.ts';
-
-export const getLogin = async ({ avatar, email, emailVerified, userName }: IRegisteredUser) => {
-  try {
-    const response = await fetch(`${import.meta.env.VITE_API_URL}/users/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ avatar, email, emailVerified, userName }),
-    });
-
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status} ${response.statusText}`);
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.error('Login error:', error);
-    throw error;
-  }
-};
-
+import { ICategoryProps } from '@/lib/types.ts';
 type TResponse = {
   data: ICategoryProps | null;
   isError: boolean;
