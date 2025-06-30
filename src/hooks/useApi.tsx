e803f81dc4512942a5449adaedcb1bc5975fdba4
Darek Mazur <dev@darekmazur.it>
2025-06-30 18:54:32 +0200

    Create hook useApi to combined all api hooks (useIdentity, usePages, useCategory)

---
+++ src/hooks/useApi.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { TResponse } from '@/lib/types.ts';
+
+export const useApi = <T,>(fn: () => Promise<TResponse<T>>) => {
+  const [data, setData] = useState<T | null>(null);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true);
+      try {
+        const res = await fn(); // Promise<TResponse<T>>
+        setIsError(res.isError);
+        setErrorMessage(res.errorMessage);
+        setData(res.data);
+      } catch (err) {
+        setErrorMessage(err instanceof Error ? `Error: ${err.message}` : 'Error: unknown error');
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [fn]);
+
+  return { data, isError, isLoading, errorMessage };
+};
