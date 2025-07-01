2cd37fc75a7961e81e32c2c8e4325e65f2564dfb
Darek Mazur <dev@darekmazur.it>
2025-07-01 20:51:55 +0200

    Create books handlers

---
+++ src/mocks/handlers/books.ts
@@ -0,0 +1,27 @@
+import { http, HttpResponse } from 'msw';
+import { db } from '@/mocks/db.ts';
+
+export const handlers = [
+  http.get(`${import.meta.env.VITE_API_URL}/books`, () => {
+    return HttpResponse.json(db.navigation.getAll());
+  }),
+
+  http.get(`${import.meta.env.VITE_API_URL}/books/:bookId`, ({ params }) => {
+    const bookId = params.bookId;
+    const idValue = Array.isArray(bookId) ? bookId[0] : bookId;
+
+    const book = db.book.findFirst({
+      where: {
+        id: {
+          equals: idValue,
+        },
+      },
+    });
+
+    if (book) {
+      return HttpResponse.json(book, { status: 200 });
+    }
+
+    return HttpResponse.json('Book not found', { status: 404 });
+  }),
+];
