0e42270a11edc74e8565233dc716bec8db3073a2
Darek Mazur <dev@darekmazur.it>
2025-07-09 10:49:39 +0200

    Test factory

---
+++ src/lib/factories/__test__/TableStrategyFactory.test.ts
@@ -0,0 +1,42 @@
+import { EPublicationType } from '@/lib/types';
+import { BookTableStrategy } from '@/lib/strategies/BookTableStrategy.ts';
+import { TableStrategyFactory } from '@/lib/factories/TableStrategyFactory.ts';
+import { ArticleTableStrategy } from '@/lib/strategies/ArticleTableStrategy.ts';
+
+describe('TableStrategyFactory.createStrategy', () => {
+  it('should return an instance of BookTableStrategy for EPublicationType.BOOK', () => {
+    const strategy = TableStrategyFactory.createStrategy(EPublicationType.BOOK);
+    expect(strategy).toBeInstanceOf(BookTableStrategy);
+  });
+
+  it('should return an ArticleTableStrategy instance for EPublicationType.ARTICLE', () => {
+    const strategy = TableStrategyFactory.createStrategy(EPublicationType.ARTICLE);
+    expect(strategy).toBeInstanceOf(ArticleTableStrategy);
+  });
+
+  it('should have methods specific to BookTableStrategy', () => {
+    const strategy = TableStrategyFactory.createStrategy(
+      EPublicationType.BOOK,
+    ) as BookTableStrategy;
+    expect(typeof strategy.getHeaders).toBe('function');
+    expect(typeof strategy.getDisplayValue).toBe('function');
+    expect(typeof strategy.getAliases).toBe('function');
+  });
+
+  it('should have methods specific to ArticleTableStrategy', () => {
+    const strategy = TableStrategyFactory.createStrategy(
+      EPublicationType.ARTICLE,
+    ) as ArticleTableStrategy;
+    expect(typeof strategy.getHeaders).toBe('function');
+    expect(typeof strategy.getDisplayValue).toBe('function');
+    expect(typeof strategy.getAliases).toBe('function');
+  });
+
+  it('should throw an error for an unsupported type', () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    expect(() => TableStrategyFactory.createStrategy('UNSUPPORTED')).toThrow(
+      'Unsupported publication type: UNSUPPORTED',
+    );
+  });
+});
