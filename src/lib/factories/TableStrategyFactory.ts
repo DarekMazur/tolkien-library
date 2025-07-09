efd1d3c6c6c18cec39c2a3bafd3db8f2dd5b9d33
Darek Mazur <dev@darekmazur.it>
2025-07-09 23:52:27 +0200

    Create factory JSDoc

--- src/lib/factories/TableStrategyFactory.ts
+++ src/lib/factories/TableStrategyFactory.ts
@@ -14,7 +14,35 @@ type StrategyForType<T extends TPublicationType> = T extends IBookProps
     ? ITableStrategy<IPublicationProps>
     : never;
 
+/**
+ * Table strategy factory for different types of publications.
+ *
+ * By using the factory pattern, it is possible to get the appropriate instance of
+ * table strategy (`ITableStrategy`) depending on the passed publication type.
+ *
+ * @template T Default publication type, from among `TPublicationType`.
+ */
 export class TableStrategyFactory {
+  /**
+   * Returns an instance of the table strategy appropriate for the publication type.
+   *
+   * @typeParam T The type of publication whose strategy is to be created.
+   *              It can be `IBookProps` or `IPublicationProps`.
+   * @param {EPublicationType} type - Publication type, such as `EPublicationType.BOOK` or `EPublicationType.ARTICLE`.
+   * @returns {StrategyForType<T>} Instance of `ITableStrategy` matched to the publication property T.
+   *
+   * @throws {Error} When the factory does not support the transferred publication type.
+   *
+   * @example
+   * // Creating a strategy for the book
+   * const bookStrategy = TableStrategyFactory.createStrategy<IBookProps>(EPublicationType.BOOK);
+   * bookStrategy.renderTable(data);
+   *
+   * @example
+   * // Create a strategy for the article
+   * const articleStrategy = TableStrategyFactory.createStrategy<IPublicationProps>(EPublicationType.ARTICLE);
+   * articleStrategy.renderTable(data);
+   */
   static createStrategy<T extends TPublicationType = TPublicationType>(
     type: EPublicationType,
   ): StrategyForType<T> {
