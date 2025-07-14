e38d90b6efac45d167fd35d516a908e6315ac8b7
Darek Mazur <dev@darekmazur.it>
2025-07-14 16:32:39 +0200

    Refactor LibraryLayout - switch location to params

--- src/layouts/LibraryLayout.tsx
+++ src/layouts/LibraryLayout.tsx
@@ -1,5 +1,5 @@
 import Wrapper from '@/components/atoms/Wrapper/Wrapper.tsx';
-import { useLocation } from 'react-router';
+import { useParams } from 'react-router';
 import Loader from '@/components/atoms/Loader/Loader.tsx';
 import CategoryPage from '@/components/pages/CategoryPage/CategoryPage.tsx';
 import { getBooksByAuthor, getCategoryBySlug } from '@/lib/getDataFromApi.ts';
@@ -8,23 +8,19 @@ import BooksPage from '@/components/pages/BooksPage/BooksPage.tsx';
 import { EPublicationType } from '@/lib/types';
 
 const LibraryLayout = () => {
-  const location = useLocation();
-  const categoryFullSlug = location.pathname.split('/').filter(Boolean);
-  const categorySlug = categoryFullSlug[categoryFullSlug.length - 1];
-  const { data: category, isLoading, isError } = useApi(() => getCategoryBySlug(categorySlug));
+  const { type } = useParams();
+  const { data: category, isLoading, isError } = useApi(() => getCategoryBySlug(type || ''));
   const { data: books, isError: booksError } = useApi(() => getBooksByAuthor('J.R.R. Tolkien'));
 
   const isValidLibraryPath = (): boolean => {
     return (
-      categoryFullSlug.length >= 2 &&
-      categoryFullSlug[0] === 'library' &&
-      Object.values(EPublicationType).includes(categoryFullSlug[1].slice(0, -1) as EPublicationType)
+      !!type && Object.values(EPublicationType).includes(type.slice(0, -1) as EPublicationType)
     );
   };
 
   const getPublicationType = (): EPublicationType | null => {
-    if (isValidLibraryPath()) {
-      return categoryFullSlug[1].slice(0, -1) as EPublicationType;
+    if (isValidLibraryPath() && type) {
+      return type.slice(0, -1) as EPublicationType;
     }
     return null;
   };
