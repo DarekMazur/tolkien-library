97086608016dcb1d6d7002d2292009f522c7577b
Darek Mazur <dev@darekmazur.it>
2025-07-15 11:00:07 +0200

    Create publications types

--- src/components/pages/PublicationPage/PublicationPage.tsx
+++ src/components/pages/PublicationPage/PublicationPage.tsx
@@ -3,21 +3,39 @@ import GenericTable from '@/components/organisms/GenericTable/GenericTable.tsx';
 import { Divider, Typography } from '@mui/material';
 
 const PublicationPage = ({ data }: { data: IPublicationProps[] }) => {
+  const partialPublications: IPublicationProps[] = data.filter((item) => item.type === 'partial');
+  const includingPublications: IPublicationProps[] = data.filter(
+    (item) => item.type === 'including',
+  );
+  const epubPublications: IPublicationProps[] = data.filter((item) => item.type === 'epub');
+
   return (
     <>
-      <Typography variant="h3">Fragmentarium</Typography>
+      <Typography variant="h2" component="h3">
+        Fragmentarium
+      </Typography>
       <Divider />
-      <GenericTable
-        data={data}
-        publicationType={EPublicationType.ARTICLE}
-        title="Items partly related to Tolkien"
-      />
-      <GenericTable
-        data={data}
-        publicationType={EPublicationType.ARTICLE}
-        title="Items that contain Tolkien articles or are partially devoted to Tolkien's works"
-      />
-      <GenericTable data={data} publicationType={EPublicationType.ARTICLE} title="E-publications" />
+      {partialPublications.length > 0 ? (
+        <GenericTable
+          data={data.filter((item) => item.type === 'partial')}
+          publicationType={EPublicationType.ARTICLE}
+          title="Items partly related to Tolkien"
+        />
+      ) : null}
+      {includingPublications.length > 0 ? (
+        <GenericTable
+          data={data.filter((item) => item.type === 'including')}
+          publicationType={EPublicationType.ARTICLE}
+          title="Items that contain Tolkien articles or are partially devoted to Tolkien's works"
+        />
+      ) : null}
+      {epubPublications.length > 0 ? (
+        <GenericTable
+          data={data.filter((item) => item.type === 'epub')}
+          publicationType={EPublicationType.ARTICLE}
+          title="E-publications"
+        />
+      ) : null}
     </>
   );
 };
