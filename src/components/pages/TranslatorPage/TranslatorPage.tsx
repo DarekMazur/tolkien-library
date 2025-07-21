057c51038cd21d06d1292f0f31f6af894f175cdb
Darek Mazur <dev@darekmazur.it>
2025-07-21 15:40:07 +0200

    Create translator page sketch

---
+++ src/components/pages/TranslatorPage/TranslatorPage.tsx
@@ -0,0 +1,13 @@
+import Wrapper from '@/components/atoms/Wrapper/Wrapper';
+import { Typography } from '@mui/material';
+
+const TranslatorPage = () => {
+  return (
+    <Wrapper>
+      <Typography variant="h2">Translator</Typography>
+      <Typography variant="h3">John Doe</Typography>
+    </Wrapper>
+  );
+};
+
+export default TranslatorPage;
