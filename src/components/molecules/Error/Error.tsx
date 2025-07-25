6eeb02291544b237dff95e3f6dcdbfacb6c874c4
Darek Mazur <dev@darekmazur.it>
2025-07-25 11:54:47 +0200

    Refactor Error components

--- src/components/molecules/Error/Error.tsx
+++ src/components/molecules/Error/Error.tsx
@@ -1,17 +1,39 @@
-import { Button, Typography } from '@mui/material';
+import { Alert, Button, Stack, Typography, AlertTitle } from '@mui/material';
 import Wrapper from '@/components/atoms/Wrapper/Wrapper.tsx';
 import { useNavigate } from 'react-router';
 
-const Error = ({ errorMessage }: { errorMessage?: string }) => {
+export interface ErrorProps {
+  errorMessage?: string;
+  errorCode?: number | string;
+  resetButtonText?: string;
+  onReset?: () => void;
+}
+
+const Error = ({ errorMessage, errorCode, resetButtonText = 'Go home', onReset }: ErrorProps) => {
   const navigate = useNavigate();
 
+  const handleReset = () => {
+    if (onReset) {
+      onReset();
+    } else {
+      navigate('/', { replace: true });
+    }
+  };
+
   return (
-    <Wrapper sx={{ display: 'flex', alignItems: 'center', gap: 3 }}>
-      <Typography variant="h3">Something went wrong...</Typography>
-      <Typography>{errorMessage ?? 'Please try again'}</Typography>
-      <Button variant="outlined" onClick={() => navigate('/')}>
-        Go home
-      </Button>
+    <Wrapper>
+      <Stack spacing={3} alignItems="center">
+        <Typography variant="h3" component="h3" tabIndex={-1}>
+          Something went wrong...
+        </Typography>
+        <Alert severity="error">
+          <AlertTitle color="error">{errorMessage ?? 'Please try again'}</AlertTitle>
+          {errorCode && <Typography variant="subtitle1">{`Code: ${errorCode}`}</Typography>}
+        </Alert>
+        <Button aria-label="Go back home" variant="outlined" onClick={handleReset}>
+          {resetButtonText}
+        </Button>
+      </Stack>
     </Wrapper>
   );
 };
