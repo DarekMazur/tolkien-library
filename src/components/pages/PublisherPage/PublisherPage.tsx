1a811d4dbf890ecb37afa1a418305edcf9274716
Darek Mazur <dev@darekmazur.it>
2025-07-28 12:41:50 +0200

    Create page sketch

---
+++ src/components/pages/PublisherPage/PublisherPage.tsx
@@ -0,0 +1,7 @@
+import Wrapper from '@/components/atoms/Wrapper/Wrapper.tsx';
+
+const PublisherPage = () => {
+  return <Wrapper>This is publisher page</Wrapper>;
+};
+
+export default PublisherPage;
