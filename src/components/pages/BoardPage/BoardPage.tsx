b2aa407fc5895f595f95669450daa242df847810
Darek Mazur <dev@darekmazur.it>
2025-08-04 08:55:04 +0200

    Create board page

---
+++ src/components/pages/BoardPage/BoardPage.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import Wrapper from '@/components/atoms/Wrapper/Wrapper.tsx';
+
+const BoardPage = () => {
+  return (
+    <Wrapper>
+      <Typography component="h2">Board Page</Typography>
+    </Wrapper>
+  );
+};
+
+export default BoardPage;
