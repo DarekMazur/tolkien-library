7706a179e1f4ba653537a4ddf1ffd16b04d16c4e
Darek Mazur <dev@darekmazur.it>
2025-08-04 09:05:52 +0200

    Add user role info

--- src/components/pages/BoardPage/BoardPage.tsx
+++ src/components/pages/BoardPage/BoardPage.tsx
@@ -1,10 +1,25 @@
-import { Typography } from '@mui/material';
-import Wrapper from '@/components/atoms/Wrapper/Wrapper.tsx';
+import { Box, Typography } from '@mui/material';
+import Wrapper from '@/components/atoms/Wrapper/Wrapper';
+import { useMe } from '@/hooks/useMe';
+import Loader from '@/components/atoms/Loader/Loader';
+import Error from '@/components/molecules/Error/Error';
 
 const BoardPage = () => {
+  const { user, isLoading } = useMe();
+
+  if (isLoading) {
+    return <Loader isLoading={isLoading} />;
+  }
+
+  if (!user) {
+    return <Error />;
+  }
+
   return (
     <Wrapper>
-      <Typography component="h2">Board Page</Typography>
+      <Typography variant="h2">Admin Panel</Typography>
+      <Typography variant="h3">You're logged in as {user.role.roleName}</Typography>
+      <Box></Box>
     </Wrapper>
   );
 };
