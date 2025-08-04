b77e5bbcc782d9e8a19e05de8e4387b1c8c38c3b
Darek Mazur <dev@darekmazur.it>
2025-08-04 10:38:37 +0200

    Style list

--- src/components/pages/BoardPage/BoardPage.tsx
+++ src/components/pages/BoardPage/BoardPage.tsx
@@ -1,4 +1,15 @@
-import { Box, List, ListItem, ListItemButton, Typography } from '@mui/material';
+import {
+  Box,
+  Link,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from '@mui/material';
+import { Link as RouterLink } from 'react-router';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 import Wrapper from '@/components/atoms/Wrapper/Wrapper';
 import { useMe } from '@/hooks/useMe';
 import Loader from '@/components/atoms/Loader/Loader';
@@ -19,22 +30,78 @@ const BoardPage = () => {
     <Wrapper>
       <Typography variant="h2">Admin Panel</Typography>
       <Typography variant="h3">You're logged in as {user.role.roleName}</Typography>
+      <Box sx={{ width: '100%', py: 4, display: 'flex', justifyContent: 'space-evenly' }}>
+        <Box>
+          <Typography variant="h3" component="h4">
+            Newest User
+          </Typography>
+          <Typography>John Doe</Typography>
+        </Box>
+        <Box>
+          <Typography variant="h3" component="h4">
+            Newest Entry
+          </Typography>
+          <Typography>
+            <Link component={RouterLink} to={'/'}>
+              Lorem ipsum
+            </Link>
+          </Typography>
+        </Box>
+      </Box>
       <Box>
-        <List>
+        <List
+          sx={{
+            width: 'fit-content',
+            minWidth: 0,
+          }}
+        >
           <ListItem>
-            <ListItemButton>Add book</ListItemButton>
+            <ListItemIcon>
+              <AddCircleIcon />
+            </ListItemIcon>
+            <ListItemButton>
+              <ListItemText primary="Add news" />
+            </ListItemButton>
           </ListItem>
           <ListItem>
-            <ListItemButton>Add Publication</ListItemButton>
+            <ListItemIcon>
+              <AddCircleIcon />
+            </ListItemIcon>
+            <ListItemButton>
+              <ListItemText primary="Add book" />
+            </ListItemButton>
           </ListItem>
           <ListItem>
-            <ListItemButton>Add Fan edition</ListItemButton>
+            <ListItemIcon>
+              <AddCircleIcon />
+            </ListItemIcon>
+            <ListItemButton>
+              <ListItemText primary="Add Publication" />
+            </ListItemButton>
           </ListItem>
           <ListItem>
-            <ListItemButton>Add Translator</ListItemButton>
+            <ListItemIcon>
+              <AddCircleIcon />
+            </ListItemIcon>
+            <ListItemButton>
+              <ListItemText primary="Add Fan edition" />
+            </ListItemButton>
           </ListItem>
           <ListItem>
-            <ListItemButton>Add Publisher</ListItemButton>
+            <ListItemIcon>
+              <AddCircleIcon />
+            </ListItemIcon>
+            <ListItemButton>
+              <ListItemText primary="Add Translator" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <AddCircleIcon />
+            </ListItemIcon>
+            <ListItemButton>
+              <ListItemText primary="Add Publisher" />
+            </ListItemButton>
           </ListItem>
         </List>
       </Box>
