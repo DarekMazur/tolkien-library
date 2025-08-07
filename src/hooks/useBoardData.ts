c2df47d4301372a959850c1e514517e5a55edbda
Darek Mazur <dev@darekmazur.it>
2025-08-07 19:17:43 +0200

    Add useBoardData docs

--- src/hooks/useBoardData.ts
+++ src/hooks/useBoardData.ts
@@ -3,6 +3,21 @@ import { useMe } from '@/hooks/useMe';
 import { IUser } from '@/lib/types';
 import { useGetLatestQuery, useGetUsersQuery } from '../../store';
 
+/**
+ * Custom hook to fetch and manage board-related data.
+ *
+ * This hook consolidates user data, the latest entry data, and the current user information,
+ * while also providing loading and error states for convenience.
+ *
+ * @returns {{
+ *   user: IUser | null;           // Current authenticated user data
+ *   latestUser: IUser | undefined; // The most recently created user from the users list
+ *   latestEntry: any;              // The latest entry data fetched from the store
+ *   isLoading: boolean;            // Indicates if any of the data fetching processes are still loading
+ *   hasError: boolean;             // Indicates if there is any error or missing required user data
+ * }}
+ */
+
 export const useBoardData = () => {
   const { data: usersData, isLoading: usersLoading, isError: usersError } = useGetUsersQuery();
   const { data: entryData, isLoading: entryLoading, isError: entryError } = useGetLatestQuery();
