3ee57485d7c348a8157152581ba334c0fc6ff608
Darek Mazur <dev@darekmazur.it>
2025-08-07 19:11:37 +0200

    Add useNewestItemData docs

--- src/hooks/useNewestItemData.ts
+++ src/hooks/useNewestItemData.ts
@@ -6,6 +6,27 @@ import { getLatest } from '@/lib/helpers/getLatest';
 import { isBook, isTranslator, isPublisher } from '@/lib/helpers/publicationsTypeGuard';
 import { createSlug } from '@/lib/helpers/createSlug';
 
+/**
+ * Custom React hook for generating display text and optional link for the newest item data based on
+ * user or entry type content.
+ *
+ * @param {'user' | 'entry'} type - The type of content being processed, either 'user' or 'entry'.
+ * @param {IUser | TPublications} [content] - The content object to derive information from. It can be either a user or publication entry.
+ *
+ * @returns {{
+ *   itemData: {
+ *     displayText: string, // Formatted text for display (e.g., username with creation date or latest entry title)
+ *     link: string | null // URL link to the related resource or null if none applicable
+ *   },
+ *   isLoading: boolean // Loading state indicating if user data is still being fetched
+ * }}
+ *
+ * @remarks
+ * - When the type is 'user', the hook formats the display text with the username plus either 'you' if it's the current user or the user's creation date.
+ * - When the type is 'entry', it determines the latest publication title and generates a link to the appropriate resource (book, translator, or publisher) if applicable.
+ * - If no relevant content is provided, appropriate fallback display text ('No users' or 'No entries') is returned.
+ */
+
 export const useNewestItemData = (type: 'user' | 'entry', content?: IUser | TPublications) => {
   const { user, isLoading } = useMe();
 
