2fead81dcf813df50a2b8fa9c10d0138eb651376
Darek Mazur <dev@darekmazur.it>
2025-08-07 19:19:56 +0200

    Add formatDate docs

--- src/lib/helpers/formatDate.ts
+++ src/lib/helpers/formatDate.ts
@@ -1,6 +1,16 @@
 import { format, isToday, isYesterday } from 'date-fns';
 import { pl } from 'date-fns/locale';
 
+/**
+ * Formats a given date as a string.
+ * Returns 'today' if the date is today,
+ * 'yesterday' if the date is yesterday,
+ * otherwise returns the date formatted as 'd MMMM yyyy HH:mm' in Polish locale.
+ *
+ * @param {Date} date - The date object to format.
+ * @returns {string} The formatted date string.
+ */
+
 export const formatDate = (date: Date): string => {
   if (isToday(date)) {
     return 'today';
